d15cf74291297bd0201eb60c260df7b9a22c3039
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-04-06 19:57:42 +0000

    deletion

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# creates a file in /tmp
-file {'holberton':
-  ensure  => 'present'
-  path    => '/tmp/holberton',
-  mode    => '0774',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
