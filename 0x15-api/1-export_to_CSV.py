cbcc8ab7c863098025ca0691e621fb8bf54823b3
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-03 20:57:20 +0000

    remove extra code,

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,17 +20,6 @@ if __name__ == "__main__":
     TASK_TITLE = []
     NUMBER_OF_DONE_TASKS = 0
 
-    for task in todo_list:
-        if task.get('completed') is True:
-            TASK_TITLE.append(task.get('title'))
-            NUMBER_OF_DONE_TASKS += 1
-    TOTAL_NUMBER_OF_TASKS = len(todo_list)
-    print("Employee {} is done with tasks({}/{}):".
-          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
-                 TOTAL_NUMBER_OF_TASKS))
-
-    for t in TASK_TITLE:
-        print("\t {}".format(t))
 
     with open('2.csv', mode='w') as employee_file:
         user_writer = csv.writer(employee_file, delimiter=',', quotechar='"',
