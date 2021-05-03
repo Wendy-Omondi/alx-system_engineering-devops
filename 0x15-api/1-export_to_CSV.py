61324f54cccc954500c34522b32e32def6c74450
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-03 19:56:51 +0000

    export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    Returns information about a TO DO list progress.
+    for a given employee ID using a given REST API.
+"""
+
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todo_list = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    USER_ID = argv[1]
+    USERNAME = user.get('username')
+
+    EMPLOYEE_NAME = user.get('name')
+    TASK_TITLE = []
+    NUMBER_OF_DONE_TASKS = 0
+
+    for task in todo_list:
+        if task.get('completed') is True:
+            TASK_TITLE.append(task.get('title'))
+            NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS = len(todo_list)
+    print("Employee {} is done with tasks({}/{}):".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS,
+                 TOTAL_NUMBER_OF_TASKS))
+
+    for t in TASK_TITLE:
+        print("\t {}".format(t))
+
+    with open('2.csv', mode='w') as employee_file:
+        user_writer = csv.writer(employee_file, delimiter=',', quotechar='"',
+                                 quoting=csv.QUOTE_ALL)
+        for task in todo_list:
+            user_writer.writerow([USER_ID, USERNAME, task.get('completed'),
+                                  task.get('title')])
