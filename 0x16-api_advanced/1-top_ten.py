d91e53824b60a742c1c10d58c0ab5f599fbd0da3
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-05-18 18:33:17 +0000

    top ten titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+"""
+Queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Returns top 10 titles"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "ALX-APIproject"}
+
+    result = requests.get(url,
+                          headers=headers,
+                          params={"limit": "10"})
+
+    if (result.status_code == 200):
+        for post in result.json()["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
